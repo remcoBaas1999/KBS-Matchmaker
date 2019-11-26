3c76b1c6534ef7d2318ee7b88299f21dedc36fa0
patrickdejong <44601122+patrickdejong@users.noreply.github.com>
2019-11-26 10:30:08 +0100

    First full version of the Main/Home screen

--- Matchmaker/MainWindow.xaml.cs
+++ Matchmaker/MainWindow.xaml.cs
@@ -18,11 +18,35 @@ namespace Matchmaker
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+        }
+
+        private void Activity1_MouseDown(object sender, MouseButtonEventArgs e) {
+            Window win2 = new Window();
+            win2.Show();
+        }
+
+
+        private void Profile1Picture1_MouseDown(object sender, MouseButtonEventArgs e) { //When clicked open profile
+            Window Profile = new Window();
+            Profile.Title = "Profile: Jeff Anderson";
+            Profile.Show();
+        }
+
+        private void Activity1Tag_MouseDown(object sender, MouseButtonEventArgs e) {
+            Window tag = new Window();
+            tag.Title = "Tag: Creative";
+            tag.Show();
         }
     }
 }
