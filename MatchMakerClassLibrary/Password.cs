88fecbb23fe5a2c02c4fcf46c549b35213718cfd
SiemBosgra <siem@bosgra.net>
2019-11-26 13:56:51 +0100

    added email to password storing

--- MatchMakerClassLibrary/Password.cs
+++ MatchMakerClassLibrary/Password.cs
@@ -10,7 +10,7 @@ namespace MatchMakerClassLibrary {
         private const int saltSize = 16;
         private const int hashSize = 16;
         private const int iterations = 100000;
-        public static void StorePassword(string password) {
+        public static void StorePassword(string email, string password) {
             //Generate salt
             RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
             byte[] salt = new byte[saltSize];
@@ -30,14 +30,14 @@ namespace MatchMakerClassLibrary {
             string hashString = Convert.ToBase64String(hash);
 
             //Store the salt (string) and the hash (string)
-            MatchmakerAPI_Client.yeetpassword(hashString, saltString);
+            MatchmakerAPI_Client.yeetpassword(email, hashString, saltString);
         }
         public static bool CheckPassword(string email, string password) {
             bool check = false;
 
             //Get salt and hash from database using email
-            string saltRetrievedString = ""; //...
-            string hashRetrievedString = ""; //...
+            string saltRetrievedString = MatchmakerAPI_Client.asksalt(email);
+            string hashRetrievedString = MatchmakerAPI_Client.askhash(email);
 
             //Convert salt to string
             byte[] saltRetrieved = Convert.FromBase64String(saltRetrievedString);
