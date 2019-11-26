7b470975eefdae5d769f7bf746066870df1c4249
patrickdejong <44601122+patrickdejong@users.noreply.github.com>
2019-11-26 12:08:51 +0100

    NewtonSoft+Userdata

--- MatchMakerClassLibrary/MatchmakerAPI_Client.cs
+++ MatchMakerClassLibrary/MatchmakerAPI_Client.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using MatchMakerClassLibrary.Models;
+using MatchMakerClassLibrary;
 
 namespace MatchMakerClassLibrary
 {
