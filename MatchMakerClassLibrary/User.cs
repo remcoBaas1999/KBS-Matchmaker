7e1ce8669ab22736ff81976d41cba54fe0409dc6
LAPTOP-ATAPCM98\marcb <marcboerdijk@hotmail.com>
2019-11-26 13:21:06 +0100

    Proto version account page and edit account page

--- MatchMakerClassLibrary/User.cs
+++ MatchMakerClassLibrary/User.cs
@@ -13,11 +13,11 @@ namespace MatchMakerClassLibrary
     {
         //properties
         public int Age { get; set; }
+        public string Bio { get; set; }
         public string PostalCode { get; set; }
-        public string Address { get; set; }
+        public string city { get; set; }
         public string ProfilePicture { get; set; }
         public string Country { get; set; }
-        public string FirstName { get; set; }
         public string LastName { get; set; }
         //attributes
         private List<string> interests = new List<string>();
