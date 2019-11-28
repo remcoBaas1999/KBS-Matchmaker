d7455a4a46be58ec2ae19ff8b3bcaf7c455845dc
patrickdejong <44601122+patrickdejong@users.noreply.github.com>
2019-11-28 10:09:49 +0100

    Interactive Profiles

--- Matchmaker/MainWindow.xaml.cs
+++ Matchmaker/MainWindow.xaml.cs
@@ -15,27 +15,36 @@ using System.Windows.Shapes;
 
 namespace Matchmaker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class HomePage : Page {
         public HomePage() {
             InitializeComponent();
 
         }
-
         private void Button_Click(object sender, RoutedEventArgs e) {
+        }
+        private void RefreshButton_MouseDown(object sender, MouseButtonEventArgs e) {
+            //Refresh recommended profiles
+
+            //Refresh Profile1
+            Profile1Tag.Content = "";
+        }
+        private void Profile1Picture1_MouseDown(object sender, MouseButtonEventArgs e) { 
+            
+            //When MouseDown on a users profilepicture
+            
+            //1. Open new page
+            Page page = new Page();
+            NavigationService.Navigate(page);
+
+            //2. Open profile and display
+            
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e) {
-        }
-        private void Profile1Picture1_MouseDown(object sender, MouseButtonEventArgs e) { //When clicked on profilepicture, open profile
-            Window Profile = new Window();
-            Profile.Title = "Profile: Jeff Anderson";
-            Profile.Show();
-
+        private void Profile1BackgroundPicture_MouseDown(object sender, MouseButtonEventArgs e) {
+            Profile1Picture1_MouseDown(sender, e);
         }
 
+        
     }
 }
