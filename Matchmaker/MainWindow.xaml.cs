66036a3e391ad4edd53e3f6e037cacb8548f2abb
Remco de Baas <32616547+remcoDeBaas99@users.noreply.github.com>
2019-11-28 14:21:23 +0100

    Mainwindow aangepast
    mainwindow is nu homepage geworden en daarna is de mainwindow weer aangemaakt.

--- Matchmaker/MainWindow.xaml.cs
+++ Matchmaker/MainWindow.xaml.cs
@@ -10,41 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Matchmaker
 {
-    public partial class HomePage : Page {
-        public HomePage() {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
             InitializeComponent();
-
         }
-        private void Button_Click(object sender, RoutedEventArgs e) {
-        }
-        private void RefreshButton_MouseDown(object sender, MouseButtonEventArgs e) {
-            //Refresh recommended profiles
-
-            //Refresh Profile1
-            Profile1Tag.Content = "";
-        }
-        private void Profile1Picture1_MouseDown(object sender, MouseButtonEventArgs e) { 
-            
-            //When MouseDown on a users profilepicture
-            
-            //1. Open new page
-            Page page = new Page();
-            NavigationService.Navigate(page);
-
-            //2. Open profile and display
-            
-
-        }
-
-        private void Profile1BackgroundPicture_MouseDown(object sender, MouseButtonEventArgs e) {
-            Profile1Picture1_MouseDown(sender, e);
-        }
-
-        
     }
 }
