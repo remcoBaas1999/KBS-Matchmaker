761fb7319b38afc55556bd5d1605c5d065c65624
patrickdejong <44601122+patrickdejong@users.noreply.github.com>
2019-11-29 10:43:19 +0100

    Homescreen menu

--- Matchmaker/MainWindow.xaml.cs
+++ Matchmaker/MainWindow.xaml.cs
@@ -45,6 +45,16 @@ namespace Matchmaker
             Profile1Picture1_MouseDown(sender, e);
         }
 
-        
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e) {
+            LoginPage loginPage = new LoginPage();
+            Page MyProfile = new Page();
+            NavigationService.Navigate(loginPage);
+        }
+
+        private void Ellipse_MouseDown_1(object sender, MouseButtonEventArgs e) {
+            //show notification page
+            Notifications notifications = new Notifications();
+            NavigationService.Navigate(notifications);
+        }
     }
 }
