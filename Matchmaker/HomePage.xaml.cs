8a5ca7b4db03ef3d1484ef3636f54a546a332289
patrickdejong <44601122+patrickdejong@users.noreply.github.com>
2019-12-02 11:18:29 +0100

    Prepared for merch with dev

--- Matchmaker/HomePage.xaml.cs
+++ Matchmaker/HomePage.xaml.cs
@@ -13,31 +13,30 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Matchmaker
-{
+
+namespace Matchmaker {
     public partial class HomePage : Page {
         public HomePage() {
+            //Start application
             InitializeComponent();
 
+            //Log into users account
         }
         private void Button_Click(object sender, RoutedEventArgs e) {
         }
         private void RefreshButton_MouseDown(object sender, MouseButtonEventArgs e) {
             //Refresh recommended profiles
-
-            //Refresh Profile1
-            Profile1Tag.Content = "";
         }
-        private void Profile1Picture1_MouseDown(object sender, MouseButtonEventArgs e) { 
-            
+        private void Profile1Picture1_MouseDown(object sender, MouseButtonEventArgs e) {
+
             //When MouseDown on a users profilepicture
-            
+
             //1. Open new page
             Page page = new Page();
             NavigationService.Navigate(page);
 
             //2. Open profile and display
-            
+
 
         }
 
@@ -45,6 +44,31 @@ namespace Matchmaker
             Profile1Picture1_MouseDown(sender, e);
         }
 
-        
+        private void myProfile(object sender, MouseButtonEventArgs e) {
+
+        }
+
+        private void Ellipse_MouseDown_1(object sender, MouseButtonEventArgs e) {
+            //show notification page
+            Notifications notifications = new Notifications();
+            notifications.Title = "Notifations";
+            NavigationService.Navigate(notifications);
+        }
+
+        private void Logout_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (MessageBox.Show("Are you sure you want to logout? All unsaved changes will be permanently lost.", "Logout", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No) {
+                //do not logout
+            }
+            else {
+                //do logout
+                LoginPage loginPage = new LoginPage();
+                NavigationService.Navigate(loginPage);
+            }
+        }
+
+        private void Settings_MouseDown(object sender, MouseButtonEventArgs e) {
+            Settings settings = new Settings();
+            NavigationService.Navigate(settings);
+        }
     }
 }
