7719fe9f0c64bff61d0305ddd3080ee82f3f9615
Guus Apeldoorn <info@guusapeldoorn.nl>
2019-12-04 14:18:47 +0100

    ImageControllerTest

---
+++ MatchmakerAPI/Controllers/ImageController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace MatchmakerAPI.Controllers
+{
+    [ApiController]
+    [Route("/image/")]
+    public class ImageController : ControllerBase
+    {
+        [HttpGet("get/id={id}")]
+        public ActionResult GetImage(string id)
+		{
+			var dir = Server.MapPath("/home/student/res/images/");
+	       	var path = Path.Combine(dir, id + ".jpg");
+	       	return base.File(path, "image/jpeg");
+		}
+	}
+}
