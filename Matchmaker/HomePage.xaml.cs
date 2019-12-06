1c5e77ed257af7305b6d4bba9d205dbe97e75a17
patrickdejong <44601122+patrickdejong@users.noreply.github.com>
2019-12-06 15:20:59 +0100

    You don't get to see your own profile on the recommended list

--- Matchmaker/HomePage.xaml.cs
+++ Matchmaker/HomePage.xaml.cs
@@ -20,13 +20,20 @@ using System.Windows.Shapes;
 namespace Matchmaker {
     public partial class HomePage : Page
     {
+        private int LoggedInUserID;
         private int FirstProfileID;
         private int SecondProfileID;
         private int ThirdProfileID;
         private int FourthProfileID;
+
         public HomePage() {
             //Start application
             InitializeComponent();
+
+            //Gather info about logged-in user
+            String email = User.email;
+            UserData activeUser = MatchmakerAPI_Client.DeserializeUserData(MatchmakerAPI_Client.GetUserData(email));
+            LoggedInUserID = activeUser.id;
 
             //Get 4 profiles on homepage
             Dictionary<String, int> Profiles = new Dictionary<string, int>();
@@ -37,30 +44,24 @@ namespace Matchmaker {
 
             Random random = new Random();
             int rnd = random.Next(0, Profiles.Count);
-            int ProfileID1 = profiles.ElementAt(rnd);
-            FirstProfileID = ProfileID1;
-            UserData user1 = MatchmakerAPI_Client.DeserializeUserData(MatchmakerAPI_Client.GetUserData(ProfileID1));
+            FirstProfileID = profiles.ElementAt(rnd);
+            while (FirstProfileID.Equals(LoggedInUserID)) {
+                rnd = random.Next(0, Profiles.Count);
+                FirstProfileID = profiles.ElementAt(rnd);
+            }
+            UserData user1 = MatchmakerAPI_Client.DeserializeUserData(MatchmakerAPI_Client.GetUserData(FirstProfileID));
             //Set name
             Profile1Tag.Content = user1.realName;
 
             //Set profile picture            
-            try {
-                string pfPic1 = $"https://145.44.233.207/images/users/{user1.profilePicture}";
-                ProfilePicture1.Fill = new ImageBrush(new BitmapImage(new Uri(pfPic1, UriKind.Absolute)));
-            }
-            catch {
-                string pfPic1 = $"https://145.44.233.207/images/users/0.jpg";
-                ProfilePicture1.Fill = new ImageBrush(new BitmapImage(new Uri(pfPic1, UriKind.Absolute)));
-            }
-
-
+            string pfPic1 = $"https://145.44.233.207/images/users/{user1.profilePicture}";
+            ProfilePicture1.Fill = new ImageBrush(new BitmapImage(new Uri(pfPic1, UriKind.Absolute)));
             //Set Cover Image
 
             //Create second profile
             rnd = random.Next(0, Profiles.Count);
-            int ProfileID2 = profiles.ElementAt(rnd);
-            SecondProfileID = ProfileID2;
-            while(FirstProfileID == SecondProfileID) {
+            SecondProfileID = profiles.ElementAt(rnd);
+            while(FirstProfileID == SecondProfileID || SecondProfileID.Equals(LoggedInUserID)) {
                 rnd = random.Next(0, Profiles.Count);
                 SecondProfileID = profiles.ElementAt(rnd);
             }
@@ -73,9 +74,8 @@ namespace Matchmaker {
 
             //Create third profile
             rnd = random.Next(0, Profiles.Count);
-            int ProfileID3 = profiles.ElementAt(rnd);
-            ThirdProfileID = ProfileID3;
-            while (ThirdProfileID == FirstProfileID || ThirdProfileID == SecondProfileID) {
+            ThirdProfileID = profiles.ElementAt(rnd);
+            while (ThirdProfileID == FirstProfileID || ThirdProfileID == SecondProfileID || ThirdProfileID.Equals(LoggedInUserID)) {
                 rnd = random.Next(0, Profiles.Count);
                 ThirdProfileID = profiles.ElementAt(rnd);
             }
@@ -89,9 +89,8 @@ namespace Matchmaker {
 
             //Create fourth profile
             rnd = random.Next(0, Profiles.Count);
-            int ProfileID4 = profiles.ElementAt(rnd);
-            FourthProfileID = ProfileID4;
-            while (FourthProfileID == FirstProfileID || FourthProfileID == SecondProfileID || FourthProfileID == ThirdProfileID) {
+            FourthProfileID = profiles.ElementAt(rnd);
+            while (FourthProfileID == FirstProfileID || FourthProfileID == SecondProfileID || FourthProfileID == ThirdProfileID || FourthProfileID.Equals(LoggedInUserID)) {
                 rnd = random.Next(0, Profiles.Count);
                 FourthProfileID = profiles.ElementAt(rnd);
             }
