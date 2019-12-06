51b3f931ea44e1f06bd2cc541e2c7289188c7fa3
Guus Apeldoorn <info@guusapeldoorn.nl>
2019-12-06 12:03:19 +0100

    test line-by-line file write

--- MatchmakerAPI/Controllers/UserController.cs
+++ MatchmakerAPI/Controllers/UserController.cs
@@ -85,8 +85,14 @@ namespace MatchmakerAPI.Controllers
 
 				users.Add(key, udata);
 
-				var text = JsonConvert.SerializeObject(users);
-				System.IO.File.WriteAllText(@"/home/student/data/users.json", text);
+				using (StreamWriter file = new StreamWriter("/home/student/data/userMap.json"))
+				{
+				    foreach (var entry in users) {
+						var text = JsonConvert.SerializeObject(entry.Value);
+						file.WriteLine("[{0} {1}]", entry.Key, text);
+					}
+				}
+				// System.IO.File.WriteAllText(@"/home/student/data/users.json", text);
 		    }
 			using (StreamReader r = new StreamReader("/home/student/data/userMap.json"))
 		    {
