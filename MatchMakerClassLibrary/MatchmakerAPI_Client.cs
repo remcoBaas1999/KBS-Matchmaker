0ab19b5a96db73c0319a34575c71e8bfb1be41e5
SiemBosgra <siem@bosgra.net>
2019-12-09 13:03:06 +0100

    Cover images - manually merged from SiemCoverImages

--- MatchMakerClassLibrary/MatchmakerAPI_Client.cs
+++ MatchMakerClassLibrary/MatchmakerAPI_Client.cs
@@ -11,33 +11,31 @@ using System.Security.Cryptography;
 using System.Net.Http;
 using System.Net.Security;
 
-namespace MatchMakerClassLibrary
-{
-    public static class MatchmakerAPI_Client
-    {
+namespace MatchMakerClassLibrary {
+    public static class MatchmakerAPI_Client {
         //public static readonly HttpClient client = new HttpClient();
         public static HttpClient client = new HttpClient();
 
-		public static UserData DeserializeUserData(string json) {
-			return JsonConvert.DeserializeObject<UserData>(json);
-		}
+        public static UserData DeserializeUserData(string json) {
+            return JsonConvert.DeserializeObject<UserData>(json);
+        }
 
         public static AuthData DeserializeAuthData(string json) {
             return JsonConvert.DeserializeObject<AuthData>(json);
         }
 
-		public static string GetUserData(int id) {
-			return Get($@"https://145.44.233.207/user/get/id={id}");
-		}
+        public static string GetUserData(int id) {
+            return Get($@"https://145.44.233.207/user/get/id={id}");
+        }
 
-		public static string GetUserData(string email) {
-			return Get($@"https://145.44.233.207/user/get/email={email}");
-		}
+        public static string GetUserData(string email) {
+            return Get($@"https://145.44.233.207/user/get/email={email}");
+        }
 
-		public static Dictionary<string, int> GetUsers() {
-			var json = Get($@"https://145.44.233.207/user/get/all");
+        public static Dictionary<string, int> GetUsers() {
+            var json = Get($@"https://145.44.233.207/user/get/all");
             return JsonConvert.DeserializeObject<Dictionary<string, int>>(json);
-		}
+        }
 
         public static async Task<AuthData> GetAuthDataAsync(string email) {
             try {
@@ -54,7 +52,7 @@ namespace MatchMakerClassLibrary
         public static string GetEventData(int id) {
             //return Get($@"https://145.44.233.207/get/event?id={id}");
             return null;
-		}
+        }
         public static async Task<bool> AuthenticateAsync(string email, string password) {
             bool check = false;
 
@@ -82,8 +80,7 @@ namespace MatchMakerClassLibrary
                     check = true;
                 }
                 return check;
-            }
-            catch (ArgumentNullException) {
+            } catch (ArgumentNullException) {
                 Console.WriteLine(GetAuthDataAsync(email));
             }
 
@@ -91,20 +88,24 @@ namespace MatchMakerClassLibrary
 
             return false;
         }
-		private static string Get(string uri)
-		{
+        private static string Get(string uri) {
             ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-		    request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
-		    using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-		    using(Stream stream = response.GetResponseStream())
-		    using(StreamReader reader = new StreamReader(stream))
-		    {
-		        return reader.ReadToEnd();
-		    }
-		}
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream)) {
+                return reader.ReadToEnd();
+            }
+        }
 
+        public static async Task<bool> PostNewCoverImageDataAsync(CoverImageData coverImageData) {
+            string uri = @"https://145.44.233.207/user/post/update/images";
+            var result = await Post(uri, coverImageData);
+            //doe wat met result
+            return true;
+        }
 
         public static async Task<bool> PostNewUserDataAsync(UserData newUserData) {
             string uri = @"https://145.44.233.207/user/post/new";
@@ -112,6 +113,7 @@ namespace MatchMakerClassLibrary
             //doe wat met result
             return true;
         }
+
         private static async Task<string> Post(string uri, object data) {
 
             string result;
@@ -124,8 +126,7 @@ namespace MatchMakerClassLibrary
         }
     }
 
-    public class UserData
-    {
+    public class UserData {
         public string email { get; set; }
         public string password { get; set; }
         public string salt { get; set; }
@@ -142,7 +143,7 @@ namespace MatchMakerClassLibrary
     }
 
     public class CoverImageData {
-        public int userid { get; set; }
+        public int userID { get; set; }
         public string imageName { get; set; }
     }
 }
