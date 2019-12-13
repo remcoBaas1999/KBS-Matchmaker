80741a32d2866ddc10d5f50a6cece7d0b2b2282d
Marc Boerdijk <marcboerdijk@hotmail.com>
2019-12-13 09:58:20 +0100

    Added start friend request

---
+++ MatchmakerAPI/Controllers/FriendRequestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchmakerAPI.Controllers
+{
+    public class FriendRequestController : Controller
+    {
+        //function to get all outbound contact requests
+
+        //functions to get all inbound contact requests
+
+        //function to confirm or deny chat request based on ID, ID and bool
+
+
+    }
+}
---
+++ MatchmakerAPI/Models/FriendRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchmakerAPI.Models
+{
+    public class FriendRequest
+    {
+        public List<int> contacts;
+        public List<int> request;
+    }
+}
