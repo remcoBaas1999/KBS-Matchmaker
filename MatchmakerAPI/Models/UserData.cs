7bc5ceef03135ed0d20198408d0baa486348d2da
Guus Apeldoorn <info@guusapeldoorn.nl>
2019-12-13 16:10:22 +0100

    makeshift dict

--- MatchmakerAPI/Models/UserData.cs
+++ MatchmakerAPI/Models/UserData.cs
@@ -23,6 +23,6 @@ namespace MatchmakerAPI
     		public string coverImage { get; set; }
     		public List<int> blockedUsers { get; set; }
         public List<int> requestFrom { get; set; }
-        public Dictionary<int, bool> contacts { get; set; }
+        public List<KeyValuePair<int, bool>> contacts { get; set; }
     }
 }
