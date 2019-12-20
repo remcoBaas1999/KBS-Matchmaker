57927f0ded893ce6741bc390bf2ef52fd1de5515
SiemBosgra <siem@bosgra.net>
2019-12-20 09:37:40 +0100

    Message models and controller edited

---
+++ MatchmakerAPI/Models/NewMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchmakerAPI.Models {
+    public class NewMessage {
+        public string chat { get; set; }
+        public Message content { get; set; }
+    }
+}
