82936cb13de7020897bd7075f02cc47063e73f5f
SiemBosgra <siem@bosgra.net>
2019-12-20 15:39:24 +0100

    Revert "change from keyvaluepair list to dictionary"
    This reverts commit 4f533a6eaf320f9151f20cf0a4809e34a22d0bb8.

--- MatchmakerAPI/Models/UserData.cs
+++ MatchmakerAPI/Models/UserData.cs
@@ -18,6 +18,6 @@ namespace MatchmakerAPI
     		public string coverImage { get; set; }
     		public List<int> blockedUsers { get; set; }
         public List<int> requestFrom { get; set; }
-        public Dictionary<string, bool> contacts { get; set; }
+        public List<KeyValuePair<int, bool>> contacts { get; set; }
     }
 }
